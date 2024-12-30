958c756d1eca7d0a1b7abd9f75b1597fad79e95e
Aditi Desai <132979412+githubcodingjoiner@users.noreply.github.com>
2024-12-30 22:50:14 +0530

    Update userModel.js

--- model/userModel.js
+++ model/userModel.js
@@ -1,10 +1,19 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema=new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String
-})
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-const UserModel=mongoose.model("user",userSchema);
-module.exports=UserModel;
+module.exports = mongoose.model("User", userSchema);
