b568528bf1caafd7a5773a748ec10c42887fcd85
Aditi Desai <132979412+githubcodingjoiner@users.noreply.github.com>
2024-12-30 23:15:42 +0530

    Add files via upload

--- model/userModel.js
+++ model/userModel.js
@@ -1,19 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose=require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+const userSchema=new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String
+})
 
-module.exports = mongoose.model("User", userSchema);
+const UserModel=mongoose.model("user",userSchema);
+module.exports=UserModel;
